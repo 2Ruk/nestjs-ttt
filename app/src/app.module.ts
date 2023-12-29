6fc0d4a88839fda244159699d053ca8e3f34d4ae
hychoi <hy.choi@sirloin.co.kr>
2023-12-29 09:43:47 +0900

    feat: typeorm setting

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { BoardModule } from './board/board.module';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [BoardModule],
+  imports: [
+    BoardModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '11223344',
+      database: 'postgres',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
