b13637e824706f0ac80e1fa9a7062f1cf2acc0b1
hychoi <hy.choi@sirloin.co.kr>
2023-12-29 09:56:19 +0900

    feat: setting domain object

--- app/src/board/entities/board.entity.ts
+++ app/src/board/entities/board.entity.ts
@@ -1,1 +1,17 @@
-export class Board {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BoardStatus } from './enum/board-status.enum';
+
+@Entity()
+export class Board {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: BoardStatus;
+}
