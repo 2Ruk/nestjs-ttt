4fdcaa3b1da54ffbf94b07debbe5f15688aad3c7
2Ruk <leerukdev@gmail.com>
2024-01-09 11:30:42 +0900

    init app

---
+++ hy/src/library/filter/http-exception.filter.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
+
+/**
+ * HttpException Catch: NestJS에서는 예외 처리를 위해 Exception Filter를 사용한다.
+ */
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+
+    response.status(status).json({
+      statusCode: status,
+      message: exception.message,
+      path: request.url,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
