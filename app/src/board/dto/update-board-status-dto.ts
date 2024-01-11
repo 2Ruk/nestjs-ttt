36d54c336b5315051bac2d0b843926352fccfa1d
2Ruk <leerukdev@gmail.com>
2024-01-11 20:40:03 +0900

    delete folder

--- app/src/board/dto/update-board-status-dto.ts
+++
@@ -1,9 +0,0 @@
-import { BoardStatus } from '../entities/enum/board-status.enum';
-import { IsEnum } from 'class-validator';
-import { Transform, Type } from 'class-transformer';
-
-export class UpdateBoardStatusDto {
-  @IsEnum(BoardStatus)
-  @Transform(({ value }) => value.toUpperCase())
-  status: BoardStatus;
-}
